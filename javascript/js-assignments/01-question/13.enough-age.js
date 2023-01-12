e6737227078c70ac9f07e9a96e0b8665504a4288
= <=>
2023-01-12 14:06:55 -0500

    old enough to drive assignment is finished

--- javascript/js-assignments/01-question/13.enough-age.js
+++ javascript/js-assignments/01-question/13.enough-age.js
@@ -1,2 +1,8 @@
 // program to check if old enough to drive
-let age = 18;
+
+let age = prompt("Enter your age:");
+if(age >= 18) {
+    alert('You are old enough to drive');
+} else {
+    alert(`Wait for ${18 - age} years to drive!`);
+}
