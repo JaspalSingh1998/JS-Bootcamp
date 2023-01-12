f50249fbd4bc10bd0279adf39bcd7d1410dd6290
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-01-12 14:22:33 -0500

    finished finished grades assignments

---
+++ javascript/js-assignments/01-question/15-student-grades.js
@@ -0,0 +1,13 @@
+let number = 87
+
+if(number >= 80 && number <= 100) {
+    console.log('A');
+}else if(number >= 70 && number <= 79) {
+    console.log('B');
+}else if(number >= 60 && number <= 69) {
+    console.log('C');
+}else if(number >= 50 && number <= 59) {
+    console.log('D');
+}else if(number >= 0 && number <= 49) {
+    console.log('F');
+}
