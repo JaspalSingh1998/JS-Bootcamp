c6d9f94eef70ea8635d846664ab383f6d34d8d6f
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-01-20 12:58:32 -0500

    finished countires

--- javascript/js-assignments/01-question/18-countries.js
+++ javascript/js-assignments/01-question/18-countries.js
@@ -1,1 +1,8 @@
-const countries = ['India', 'CANADA', 'USA', 'New Zeland', 'Australia', 'Cuba', 'ETHIOPIA']
+const countries = ['India', 'CANADA', 'USA', 'New Zeland', 'Australia', 'Cuba', 'Ethiopia']
+
+
+if (countries.includes('Ethiopia')) {
+    console.log('ETHIOPIA');
+} else {
+    countries.push('Ethiopia');
+}
